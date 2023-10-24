1f4749c258065262eea57a999f872c977b451f6a
TheClashFruit <admin@theclashfruit.me>
2023-10-24 20:14:25 +0200

    fix: left in useless thing

--- pages/post/[permalink].js
+++ pages/post/[permalink].js
@@ -21,7 +21,6 @@ export default function Post({ postData }) {
       <div className="max-w-5xl pb-4 lg:mx-auto max-lg:px-4 lg:px-0">
         <AdBanner
           data-ad-slot="6363427510"
-          data-ad-layout-key="-62+di+47-55+1l"
           data-ad-format="auto"
           data-full-width-responsive="true"
         />
