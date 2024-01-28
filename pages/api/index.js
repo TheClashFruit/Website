e5224f2768261f90bf222a8793f0d7a68b7a704b
TheClashFruit <clash.fruit123@gmail.com>
2024-01-28 19:05:23 +0100

    feat: start adding db stuff

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,34 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import fs from 'fs';
+import Database from '@/lib/database';
 
 export default function handler(req, res) {
-  try {
-    const buildId = fs.readFileSync('.next/BUILD_ID', 'utf8');
-
-    res.status(200).json({
-      latestVersion: 0,
-      buildId: buildId ,
-      versions: [
-        {
-          name: '1.0.0',
-          swagger: 'swagger.json',
-        }
-      ]
-    });
-  } catch (err) {
-    console.log(err);
-
-    res.status(200).json({
-      latestVersion: 0,
-      buildId: 'dev',
-      swagger: '/swagger.json',
-      versions: [
-        {
-          name: '1.0.0',
-          path: '/api/v1/',
-        }
-      ]
-    });
+  const resp = {
+    latest_version: 0,
+    build_id: null,
+    swagger: {
+      data: 'https://cdn-new.theclashfruit.me/data/swagger.json',
+      ui: 'https://theclashfruit.me/docs/swagger',
+    },
+    versions: [
+      {
+        name: '1.0.0',
+        path: '/api/v1',
+        deprecated: false
+      }
+    ]
   }
+
+  const db = new Database();
+
+  try {
+    resp.build_id = fs.readFileSync('.next/BUILD_ID', 'utf8');
+  } catch (err) {
+    resp.build_id = 'dev';
+  }
+
+  res.status(200).json(resp);
 }
---
+++ pages/api/v1/posts.js
@@ -0,0 +1,16 @@
+import Database from '@/lib/Database';
+
+export default async function handler(req, res) {
+  const db = new Database();
+
+  const query = req.query;
+
+  const posts = await db.getPosts(query.offset, query.limit);
+
+  res.status(200).json({
+    data: posts,
+    offset: parseInt(query.offset) || 0,
+    limit: parseInt(query.limit) || 10,
+    total: posts.length
+  });
+}
