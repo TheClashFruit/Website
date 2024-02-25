8d4e4664ce9b86c75bf5ee59000a9fdcedee5aa9
TheClashFruit <clash.fruit123@gmail.com>
2024-02-25 17:59:06 +0100

    feat: a bit of federation stuff

--- pages/api/webfinger.js
+++ pages/api/webfinger.js
@@ -1,26 +1,43 @@
-import Database from '@/lib/Database';
+import Database from '../../lib/Database';
 
 export default async function handler(req, res) {
   const db = new Database();
 
   const query = req.query;
 
-  const finger = {
-    subject: 'acct:me@theclashfruit.me',
-    links: [
-      {
-        rel: 'https://webfinger.net/rel/profile-page',
-        type: 'text/html',
-        href: 'https://theclashfruit.me'
-      },
-      {
-        rel: 'https://packetizer.com/rel/blog',
-        href: 'https://theclashfruit.me/blog'
-      }
-    ]
-  };
+  if (query.resource.startsWith('acct:')) {
+    const user = await db.getUserFromUsername(query.resource.split(':')[1].split('@')[0]);
 
-  if (query.resource === 'acct:me@theclashfruit.me') {
+    if (user === null) {
+      res.status(404).send();
+
+      return;
+    }
+
+    const finger = {
+      subject: query.resource,
+      aliases: [
+        `https://theclashfruit.me/u/${user.username}`
+      ],
+      links: [
+        {
+          rel: 'http://webfinger.net/rel/profile-page',
+          type: 'text/html',
+          href: `https://theclashfruit.me/u/${user.username}`
+        },
+        {
+          rel: 'self',
+          type: 'application/activity+json',
+          href: `https://theclashfruit.me/api/activitypub/${user.username}/actor`
+        },
+        {
+          rel: 'http://webfinger.net/rel/avatar',
+          type: 'image/png',
+          href: user.profile_picture
+        }
+      ]
+    };
+
     res.status(200).json(finger);
   } else {
     res.status(404).send();
