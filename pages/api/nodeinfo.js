535220eddca93198a9c3c169bdaa3d841f98c28a
TheClashFruit <clash.fruit123@gmail.com>
2024-03-03 12:34:02 +0100

    fix: missed an `await`

--- pages/api/nodeinfo.js
+++ pages/api/nodeinfo.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
         activeHalfyear: 1,
         activeMonth: 1
       },
-      localPosts: db.getPostCount(),
+      localPosts: await db.getPostCount(),
       localComments: 0
     },
     metadata: {}
