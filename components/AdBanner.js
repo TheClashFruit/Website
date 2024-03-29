4a4c0a15b1bacbe313bcd1c59783eb902d949ccb
TheClashFruit <clash.fruit123@gmail.com>
2024-03-29 14:45:51 +0100

    feat: remove in house ads till I make it stable

--- components/AdBanner.js
+++ components/AdBanner.js
@@ -11,8 +11,8 @@ export default function AdBanner(props) {
   const googleAdRef = useRef(null);
 
   const [ dialogOpen, setDialogOpen ] = useState(false);
-  const [ adData, setAdData ] = useState();
-  const [ ads, setAds ] = useState();
+
+  const [ adBlocked, setAdBlocked ] = useState(false);
 
   useEffect(() => {
     try {
@@ -25,6 +25,8 @@ export default function AdBanner(props) {
       fetch(new Request('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js')).catch(_ => googleAdRef.current.setAttribute('data-blocked', 'yes'));
     } catch (e) {
       googleAdRef.current.setAttribute('data-blocked', 'yes');
+
+      setAdBlocked(true);
     }
 
     document.addEventListener('load', () => {
@@ -35,28 +37,14 @@ export default function AdBanner(props) {
     });
   }, [ googleAdRef ]);
 
-  useEffect(() => {
-    fetch('/api/v2/ads/random')
-      .then(res => res.json())
-      .then(res => {
-        setAdData(res);
-
-        fetch('/api/v2/ads/impression', { method: 'POST', body: JSON.stringify({ id: res.id }) });
-      });
-  }, [ setAdData ]);
-
   return (
     <>
       <Card className={styles.adBanner}>
-        {adData &&
-          <div className={styles.adContent} dangerouslySetInnerHTML={{__html: adData.html}}/>
-        }
+        <div className={styles.adContent}>
+          <h3>Support Me</h3>
 
-        {!adData &&
-          <div className={styles.adContent}>
-            <h3>No Ad Loaded.</h3>
-          </div>
-        }
+          <p style={{marginBottom: 0}}>If you like my work you can support me on <Link href="https://ko-fi.com/TheClashFruit">Ko-Fi</Link> or on <Link href="https://github.com/sponsors/TheClashFruit/">GitHub Sponsors</Link>.</p>
+        </div>
 
         <div className={styles.adGoogle}>
           <ins className="adsbygoogle" style={{
@@ -74,18 +62,11 @@ export default function AdBanner(props) {
         <Dialog title="About Ads" closeAction={() => { setDialogOpen(false); }}>
           <h3 style={{marginBottom: '0.5rem'}}>Google AdSense</h3>
 
-          <p style={{marginBottom: '0.5rem'}}>This is the main method ads are shown. For information about the ad shown click on the &quot;AdChoises&quot; button. Will be phased out once I get enough in-house ads</p>
+          <p style={{marginBottom: '0.5rem'}}>This is the main method ads are shown. For information about the ad shown click on the &quot;AdChoises&quot; button. Will be phased out once I get enough in-house ads.</p>
 
-          <h3 style={{marginBottom: '0.5rem'}}>In House Ads</h3>
+          <h3 style={{marginBottom: '0.5rem'}}>In-house Ads</h3>
 
-          <p style={{marginBottom: '0.5rem'}}>&quot;Fallback&quot; method. These ads are randomly selected from the ad pool and the only tracking for them is when you click one and impressions.</p>
-
-          <p style={{marginBottom: '0.5rem'}}>About the currently shown ad:</p>
-
-          <div style={{marginBottom: '0'}}>
-            <b>Advertiser:</b>
-            <p style={{marginBottom: '0'}}>{adData.advertiser}</p>
-          </div>
+          <p>Coming Soon!</p>
         </Dialog>
       }
     </>
